bc55db227f37878bcb3126c40a37d0defa497ca5
Lê Quốc Thước <102453854+thuocle@users.noreply.github.com>
2023-06-28 22:15:12 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QuanLyHoDan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HoDan a = new HoDan();
+            a.NhapThongTin();
+            a.HienThiThongTin();
+        }
+    }
+}
